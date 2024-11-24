c14b15e4f27e6e7060441816def191c652382bb9
mashukak <mashenkast12@gmail.com>
2024-11-24 14:21:02 +0100

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,15 @@
-const elementInput = document.querySelector('#input');
-const elementOutput = document.querySelector('#output');
 
-elementInput.addEventListener('input', update);
+const elementInput = document.querySelector('#name-input');
+const elementOutput = document.querySelector('#name-output');
+
+
+elementInput.addEventListener('name-input', update);
 
 
 function update(event) {
 
   const inputValue = event.target.value.trim();
 
-  elementOutput.textContent = inputValue;
+
+  elementOutput.textContent = inputValue.length > 0 ? inputValue : 'Anonymous';
 }
